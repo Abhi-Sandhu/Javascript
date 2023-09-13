a9cfcbe61338ffd09e0054348b9bad558f776202
Abhi-Sandhu <105337430+Abhi-Sandhu@users.noreply.github.com>
2023-09-13 19:10:14 +0000

    Add Datatypes.js

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert (3 + 3 )  // we are using nodejs, not browser
+
+
+console.log(3
+     + 
+     3) // readability should be maintained 
+
+let Name = "Abhi"
+let age = 21
+let isLoggedIn = false
+
+// Primitive Datatypes
+
+//   number => 2 to power 53
+//   Bigint
+//   string => ""
+//   boolean => true/false 
+//   null => Stand-alone value
+//   undefined => jo abhi tkk define nhi hua hai
+//   Symbol => unique
+
+
+console.log(typeof age);
+// typeof    // give the type of anything.
+
+
+console.log(typeof undefined); // Undefined
+console.log(typeof null);   // object
