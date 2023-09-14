158a1af0a137ab15539aed03790b1dffd070c5fc
Abhi-Sandhu <105337430+Abhi-Sandhu@users.noreply.github.com>
2023-09-14 18:56:48 +0000

    Conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// When converting to Number: 
+    // "33" => NaN (not a number)
+    // null => 0
+    // undefined => NaN
+    // true => 1;   false => 0;
+
+
+
+let isLoggedIn = "abhi"
+let valueInBoolean = Boolean(isLoggedIn)
+console.log(valueInBoolean);
+
+
+// When converting to boolean:
+     // 1 => true;    0 => false
+     // "" => false;   
+     // "Abhi" => true;
+
+
+
+let number = 33
+let valueInString = String(number)
+console.log(valueInString);
+console.log(typeof valueInString);
