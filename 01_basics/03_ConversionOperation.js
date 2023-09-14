4ca06a32527d364abcce579bbdc7e2910b93ea0c
Abhi-Sandhu <105337430+Abhi-Sandhu@users.noreply.github.com>
2023-09-14 19:31:49 +0000

    Operations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -18,7 +18,7 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = "abhi"
 let valueInBoolean = Boolean(isLoggedIn)
-console.log(valueInBoolean);
+// console.log(valueInBoolean);
 
 
 // When converting to boolean:
@@ -30,5 +30,67 @@ console.log(valueInBoolean);
 
 let number = 33
 let valueInString = String(number)
-console.log(valueInString);
-console.log(typeof valueInString);
+// console.log(valueInString);
+// console.log(typeof valueInString);
+
+
+
+
+//  **********************Operations**************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);   // add
+// console.log(2-2);   // sub
+// console.log(2*2);   // multiply
+// console.log(2**3);  // power
+// console.log(2/3);   // divide
+// console.log(2%3);   // modulus
+
+
+
+let str1 = "Hello"
+let str2  = " Abhi"
+// console.log(str1+str2);
+
+
+// String ya number cho jo pehla aauga ohde ch change hoju output
+        // console.log("1" + 2);
+        // console.log(1 + "2");
+        // console.log("1" + 2 + 2);   
+        // console.log(1 + 2 + "2");
+
+
+// Some Examples of wrong code practices
+
+    // console.log(+true);
+    // console.log(+"");
+
+
+    // let num1, num2, num3;
+    // num1 = num2 = num3 = 2+2
+    // console.log(num1);
+
+
+
+
+
+                // let gameCounter = 100
+                // let num =  gameCounter++;
+                // console.log(num);
+                // console.log(gameCounter);
+                
+                // let gameCounter = 100
+                // let num =  ++gameCounter;
+                // console.log(num);
+                // console.log(gameCounter);
+                
+
+
+
+
+
+
